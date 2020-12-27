3990fa0f51b50b09232433c02bdd234907ba8b07
ibbeadam <75492731+ibbeadam@users.noreply.github.com>
2020-12-27 14:50:00 +0500

    index.php updated

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Grade System</title>
+    <title>Student Grading System</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
